8ea3c84e271f25b190c0e7d565a156eb1fc34725
wateos <mat.oliver.strong@gmail.com>
2021-07-31 22:55:09 -0300

    config local

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -16,12 +16,12 @@ export class AuthService {
   ) { }
 
   entrar(userLogin: UserLogin): Observable<UserLogin>{
-    return this.http.post<UserLogin>('https://blogwateos.herokuapp.com/usuarios/logar', userLogin)
+    return this.http.post<UserLogin>('http://localhost:8080/usuarios/logar', userLogin)
 
   }
 
   cadastrar(user: User): Observable<User>{
-    return this.http.post<User>('https://blogwateos.herokuapp.com/usuarios/cadastrar', user)
+    return this.http.post<User>('http://localhost:8080/usuarios/cadastrar', user)
   }
 
   logado(){
--- src/app/service/tema.service.ts
+++ src/app/service/tema.service.ts
@@ -22,23 +22,23 @@ export class TemaService {
   }
 
   getAllTema(): Observable<Tema[]>{
-    return this.http.get<Tema[]>('https://blogwateos.herokuapp.com/tema', this.token)
+    return this.http.get<Tema[]>('http://localhost:8080/tema', this.token)
 
   }
 
   getByIdTema(id: number): Observable<Tema>{
-    return this.http.get<Tema>(`https://blogwateos.herokuapp.com/tema/${id}`, this.token)
+    return this.http.get<Tema>(`http://localhost:8080/tema/${id}`, this.token)
   }
 
   postTema(tema: Tema): Observable<Tema>{
-    return this.http.post<Tema>('https://blogwateos.herokuapp.com/tema', tema, this.token)
+    return this.http.post<Tema>('http://localhost:8080/tema', tema, this.token)
   }
 
   putTema(tema: Tema): Observable<Tema>{
-    return this.http.put<Tema>('https://blogwateos.herokuapp.com/tema', tema, this.token)
+    return this.http.put<Tema>('http://localhost:8080/tema', tema, this.token)
   }
 
   deleteTema(id: number){
-    return this.http.delete(`https://blogwateos.herokuapp.com/tema/${id}`, this.token)
+    return this.http.delete(`http://localhost:8080/tema/${id}`, this.token)
   }
 }
